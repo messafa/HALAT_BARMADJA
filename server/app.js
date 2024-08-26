0a1cfb841e1a8d48b372e100685c26a579a45de4
hisoka <172132016926@univ-chlef.dz>
2024-08-26 11:36:10 -0700

    chore: Update server dependencies and middleware for improved security and error handling

--- server/app.js
+++ server/app.js
@@ -6,18 +6,27 @@ const authRoutes = require("./routes/authRoutes");
 const milkRoutes = require("./routes/milkProductionRoutes");
 const examRoutes = require("./routes/examRoutes");
 const birthRoutes = require("./routes/birthRoutes");
+const securityMiddleware = require("./middlewares/securityMiddleware");
+const errorHandler = require("./middlewares/errorHandler");
 
 require("dotenv").config();
 
 const app = express();
+securityMiddleware(app);
+
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/cows", cowRoutes);      // Done
-app.use("/auth", authRoutes);     // Done
-app.use("/milk", milkRoutes);     // Done
-app.use("/exam", examRoutes);     // Done
-app.use("/births", birthRoutes);  // Todo: work on this
+app.use("/cows", cowRoutes);
+app.use("/auth", authRoutes);
+app.use("/milk", milkRoutes);
+app.use("/exam", examRoutes);
+app.use("/births", birthRoutes);
+
+
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
