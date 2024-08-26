93680ef7bc5dbf132ede2b84a41b0f3902eeb454
hisoka <172132016926@univ-chlef.dz>
2024-08-25 18:09:03 -0700

    chore: Update npm dependencies for axios, cors, express, express-validator, http-status-codes, jsonwebtoken, short-unique-id, Chakra UI, Emotion, and Framer Motion

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,20 +1,38 @@
 const { readJSONFile } = require("../utils/jsonUtils");
 const jwtUtils = require("../utils/jwtUtils");
+const { StatusCodes } = require("http-status-codes");
 
-exports.login = (req, res) => {
-  const { username, password } = req.body;
+const argon2 = require("argon2");
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
   const users = readJSONFile("users.json");
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
+  const user = users.find((u) => u.email === email);
+
+  if (user && (await argon2.verify(user.password, password))) {
+    const token = jwtUtils.generateToken({ id: user.id, email: user.email });
+    res.status(StatusCodes.OK).send({ token });
+  } else {
+    res.status(StatusCodes.UNAUTHORIZED).send("Invalid credentials.");
+  }
+};
+
+exports.register = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  const users = readJSONFile("users.json");
+  const user = users.find((u) => u.email === email);
 
   if (user) {
-    const token = jwtUtils.generateToken({
-      id: user.id,
-      username: user.username,
-    });
-    res.status(200).json({ token });
+    res.status(StatusCodes.BAD_REQUEST).send("User already exists.");
   } else {
-    res.status(400).send("Invalid credentials.");
+    const hashedPassword = await argon2.hash(password);
+    const newUser = {
+      id: users.length + 1,
+      fullName,
+      email,
+      password: hashedPassword,
+    };
+    users.push(newUser);
+    res.status(StatusCodes.CREATED).send(newUser);
   }
 };
--- server/controllers/cowController.js
+++ server/controllers/cowController.js
@@ -1,16 +1,57 @@
 const { readJSONFile, writeJSONFile } = require("../utils/jsonUtils");
+const ShortUniqueId = require("short-unique-id");
+const uid = new ShortUniqueId({
+  length: 8,
+  dictionary: "number",
+});
 
-exports.registerCow = (req, res) => {
+exports.getCows = (req, res) => {
   const cows = readJSONFile("cows.json");
-  const newCow = { id: Date.now(), ...req.body };
-  cows.push(newCow);
-  writeJSONFile("cows.json", cows);
-  res.status(201).send("Cow registered successfully");
+  res.send(cows);
 };
 
-exports.getCowDetails = (req, res) => {
+exports.getCow = (req, res) => {
   const cows = readJSONFile("cows.json");
   const cow = cows.find((c) => c.id === parseInt(req.params.id));
-  if (!cow) return res.status(404).send("Cow not found");
-  res.status(200).json(cow);
+  if (cow) {
+    res.send(cow);
+  } else {
+    res.status(404).send("Cow not found.");
+  }
+};
+
+exports.addCow = (req, res) => {
+  const cows = readJSONFile("cows.json");
+  const newCow = {
+    id: parseInt(uid()),
+    addedBy: req.user,
+    ...req.body,
+  };
+  cows.push(newCow);
+  writeJSONFile("cows.json", cows);
+  res.status(201).send(newCow);
+};
+
+exports.updateCow = (req, res) => {
+  const cows = readJSONFile("cows.json");
+  const cowIndex = cows.findIndex((c) => c.id === parseInt(req.params.id));
+  if (cowIndex !== -1) {
+    const updatedCow = { ...cows[cowIndex], ...req.body };
+    cows[cowIndex] = updatedCow;
+    writeJSONFile("cows.json", cows);
+    res.send(updatedCow);
+  } else {
+    res.status(404).send("Cow not found.");
+  }
+};
+
+exports.deleteCow = (req, res) => {
+  const cows = readJSONFile("cows.json");
+  const filteredCows = cows.filter((c) => c.id !== parseInt(req.params.id));
+  if (filteredCows.length < cows.length) {
+    writeJSONFile("cows.json", filteredCows);
+    res.send("Cow deleted.");
+  } else {
+    res.status(404).send("Cow not found.");
+  }
 };
