cb0aa5a11e6b35826525329c57fcdaf677d1f8ab
hisoka <172132016926@univ-chlef.dz>
2024-08-26 14:49:58 -0700

    Login & register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,18 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
+import LoginPage from './pages/LoginPage';
 
 function App() {
+  const url = window.location.href.split("/")[3];
   return (
     <Router>
-      <Navbar />
+      {/* if url has login dont diisplay navbar */}
+      {url.startsWith("login") ? null : <Navbar />}
+      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,144 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from "@chakra-ui/react";
+
+const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <ChakraProvider>
+      <Box
+        maxW="md"
+        mx="auto"
+        mt="10"
+        p="6"
+        borderRadius="lg"
+        bg="rgba(255, 255, 255, 0.2)"
+        boxShadow="lg"
+        backdropFilter="blur(10px)"
+      >
+        <Box textAlign="center" py="10" bg="transparent" position="relative">
+          <Text
+            fontSize="6xl"
+            fontFamily="cursive"
+            fontWeight="bold"
+            color="transparent"
+            style={{
+              backgroundImage: "url('/pic00.jpg')",
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundClip: "text",
+              WebkitBackgroundClip: "text",
+              textFillColor: "transparent",
+              WebkitTextFillColor: "transparent",
+            }}
+          >
+            Milki
+          </Text>
+        </Box>
+        <Tabs isFitted color={'white'} variant="enclosed" border={'teal'}>
+          <TabList>
+            <Tab>Login</Tab>
+            <Tab>Register</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Box as="form">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  mb="4"
+                  borderColor="teal.500"
+                />
+                <InputGroup size="md" mb="4">
+                  <Input
+                    pr="4.5rem"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    borderColor="teal.500"
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button
+                      h="1.75rem"
+                      size="sm"
+                      onClick={handlePasswordVisibility}
+                    >
+                      {showPassword ? (
+                        <FaEyeSlash color="teal" />
+                      ) : (
+                        <FaEye color="teal" />
+                      )}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <Button colorScheme="teal" type="submit">
+                  Login
+                </Button>
+              </Box>
+            </TabPanel>
+            <TabPanel>
+              <Box as="form">
+                <Input
+                  placeholder="Name"
+                  type="text"
+                  mb="4"
+                  borderColor="teal.500"
+                />
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  mb="4"
+                  borderColor="teal.500"
+                />
+                <InputGroup size="md" mb="4">
+                  <Input
+                    pr="4.5rem"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    borderColor="teal.500"
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button
+                      h="1.75rem"
+                      size="sm"
+                      onClick={handlePasswordVisibility}
+                    >
+                      {showPassword ? (
+                        <FaEyeSlash color="teal" />
+                      ) : (
+                        <FaEye color="teal" />
+                      )}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <Button colorScheme="teal" type="submit">
+                  Register
+                </Button>
+              </Box>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </ChakraProvider>
+  );
+};
+
+export default LoginPage;
