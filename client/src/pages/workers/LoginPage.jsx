d7fe1fa59e40d54306d6d63471bdcb2bbba9360f
hisoka <172132016926@univ-chlef.dz>
2024-08-27 10:43:20 -0700

    finiching ui

--- client/src/pages/workers/LoginPage.jsx
+++ client/src/pages/workers/LoginPage.jsx
@@ -23,122 +23,132 @@ const LoginPage = () => {
   };
 
   return (
-    <ChakraProvider
-     // I want add a background image to the login page
-
-    >
+    <ChakraProvider>
       <Box
-        maxW="md"
-        mx="auto"
-        mt="10"
-        p="6"
-        borderRadius="lg"
-        bg="rgba(255, 255, 255, 0.2)"
-        boxShadow="lg"
-        backdropFilter="blur(10px)"
+        width={"100%"}
+        height={"100vh"}
+        backgroundColor={"red"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        display={"flex"}
+        flexDirection={"column"}
+        backgroundImage="url('/pic08.jpg')"
+        backgroundSize="cover"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
       >
-        <Box textAlign="center" py="10" bg="transparent" position="relative">
-          <Text
-            fontSize="6xl"
-            fontFamily="cursive"
-            fontWeight="bold"
-            color="transparent"
-            style={{
-              backgroundImage: "url('/pic00.jpg')",
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-              backgroundClip: "text",
-              WebkitBackgroundClip: "text",
-              textFillColor: "transparent",
-              WebkitTextFillColor: "transparent",
-            }}
-          >
-            Milki
-          </Text>
+        <Box
+          maxW="md"
+          mx="auto"
+          p="6"
+          borderRadius="lg"
+          bg="rgba(2, 2, 2, 0.5)"
+          boxShadow="lg"
+          backdropFilter="blur(5px)"
+        >
+          <Box textAlign="center" py="10" bg="transparent" position="relative">
+            <Text
+              fontSize="6xl"
+              fontFamily="cursive"
+              fontWeight="bold"
+              color="transparent"
+              style={{
+                backgroundImage: "url('/pic00.jpg')",
+                backgroundSize: "contain",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundClip: "text",
+                WebkitBackgroundClip: "text",
+                textFillColor: "transparent",
+                WebkitTextFillColor: "transparent",
+              }}
+            >
+              Milki
+            </Text>
+          </Box>
+          <Tabs isFitted color={"white"} variant="enclosed" border={"teal"}>
+            <TabList>
+              <Tab>Login</Tab>
+              <Tab>Register</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Box as="form">
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    mb="4"
+                    borderColor="teal.500"
+                  />
+                  <InputGroup size="md" mb="4">
+                    <Input
+                      pr="4.5rem"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"
+                      borderColor="teal.500"
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Button
+                        h="1.75rem"
+                        size="sm"
+                        onClick={handlePasswordVisibility}
+                      >
+                        {showPassword ? (
+                          <FaEyeSlash color="teal" />
+                        ) : (
+                          <FaEye color="teal" />
+                        )}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                  <Button colorScheme="teal" type="submit">
+                    Login
+                  </Button>
+                </Box>
+              </TabPanel>
+              <TabPanel>
+                <Box as="form">
+                  <Input
+                    placeholder="Name"
+                    type="text"
+                    mb="4"
+                    borderColor="teal.500"
+                  />
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    mb="4"
+                    borderColor="teal.500"
+                  />
+                  <InputGroup size="md" mb="4">
+                    <Input
+                      pr="4.5rem"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"
+                      borderColor="teal.500"
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Button
+                        h="1.75rem"
+                        size="sm"
+                        onClick={handlePasswordVisibility}
+                      >
+                        {showPassword ? (
+                          <FaEyeSlash color="teal" />
+                        ) : (
+                          <FaEye color="teal" />
+                        )}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                  <Button colorScheme="teal" type="submit">
+                    Register
+                  </Button>
+                </Box>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </Box>
-        <Tabs isFitted color={'white'} variant="enclosed" border={'teal'}>
-          <TabList>
-            <Tab>Login</Tab>
-            <Tab>Register</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Box as="form">
-                <Input
-                  placeholder="Email"
-                  type="email"
-                  mb="4"
-                  borderColor="teal.500"
-                />
-                <InputGroup size="md" mb="4">
-                  <Input
-                    pr="4.5rem"
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Password"
-                    borderColor="teal.500"
-                  />
-                  <InputRightElement width="4.5rem">
-                    <Button
-                      h="1.75rem"
-                      size="sm"
-                      onClick={handlePasswordVisibility}
-                    >
-                      {showPassword ? (
-                        <FaEyeSlash color="teal" />
-                      ) : (
-                        <FaEye color="teal" />
-                      )}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <Button colorScheme="teal" type="submit">
-                  Login
-                </Button>
-              </Box>
-            </TabPanel>
-            <TabPanel>
-              <Box as="form">
-                <Input
-                  placeholder="Name"
-                  type="text"
-                  mb="4"
-                  borderColor="teal.500"
-                />
-                <Input
-                  placeholder="Email"
-                  type="email"
-                  mb="4"
-                  borderColor="teal.500"
-                />
-                <InputGroup size="md" mb="4">
-                  <Input
-                    pr="4.5rem"
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Password"
-                    borderColor="teal.500"
-                  />
-                  <InputRightElement width="4.5rem">
-                    <Button
-                      h="1.75rem"
-                      size="sm"
-                      onClick={handlePasswordVisibility}
-                    >
-                      {showPassword ? (
-                        <FaEyeSlash color="teal" />
-                      ) : (
-                        <FaEye color="teal" />
-                      )}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <Button colorScheme="teal" type="submit">
-                  Register
-                </Button>
-              </Box>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
       </Box>
     </ChakraProvider>
   );
